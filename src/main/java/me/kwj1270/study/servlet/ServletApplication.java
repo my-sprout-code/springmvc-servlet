27f8c1045d0fffbf06dc68581ad48d7b3fe1d336
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-04 01:07:03 +0900

    feat:서블릿:Hello 서블릿

--- src/main/java/me/kwj1270/study/servlet/ServletApplication.java
+++ src/main/java/me/kwj1270/study/servlet/ServletApplication.java
@@ -2,7 +2,10 @@ package me.kwj1270.study.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// 스프링이 자동으로 현재 패키지부터 하위 패키지의 서블릿을 찾아서 등록한다.
+@ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
