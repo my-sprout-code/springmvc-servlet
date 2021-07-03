e5b8bf101240142b107985c835bbdf059ae208a9
kwj1270 <kwj1270@kwj1270ui-MacBookPro.local>
2021-07-04 00:40:33 +0900

    feat:init

---
+++ src/main/java/me/kwj1270/study/servlet/ServletApplication.java
@@ -0,0 +1,13 @@
+package me.kwj1270.study.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletApplication.class, args);
+    }
+
+}
